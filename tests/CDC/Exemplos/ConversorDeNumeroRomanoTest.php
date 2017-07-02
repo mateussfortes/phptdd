0c23b52711419c31079fbdb428b4ce9c9393a15e
Mateus Fortes <mateussilveirafortes@gmail.com>
2017-07-01 22:37:54 -0300

    4.1 4.1 O problema do cálculo de salário

---
+++ tests/CDC/Exemplos/ConversorDeNumeroRomanoTest.php
@@ -0,0 +1,25 @@
+<?php
+  namespace CDC\Exemplos;
+  require "./vendor/autoload.php";
+  use CDC\Exemplos\ConversorDeNumeroRomano;
+  use PHPUnit_Framework_TestCase as PHPUnit;
+
+  class ConversorDeNumeroRomanoTest extends PHPUnit
+  {
+
+    public function testDeveEntenderOSimboloI()
+    {
+      $romano = new ConversorDeNumeroRomano();
+      $numero = $romano->converte("I");
+      $this->assertEquals(1, $numero);
+    }
+
+    public function testeDeveEntenderOSimboloV()
+    {
+      $romano = new ConversorDeNumeroRomano();
+      $numero = $romano->converte("V");
+      $this->assertEquals(5, $numero);
+    }
+
+  }
+?>
